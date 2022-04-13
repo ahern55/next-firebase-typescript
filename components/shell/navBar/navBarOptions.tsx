8d1eba28544afd174928b87cdec2721bdc7cf7b8
Jason Ahern <jason.ahern53@gmail.com>
2022-04-13 14:22:06 -0400

    firebase configured

---
+++ components/shell/navBar/navBarOptions.tsx
@@ -0,0 +1,40 @@
+export default function NavBarOptions() {
+  return (
+    <>
+      <a
+        href="#"
+        className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+      >
+        Dashboard
+      </a>
+
+      <a
+        href="#"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+      >
+        Team
+      </a>
+
+      <a
+        href="#"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+      >
+        Projects
+      </a>
+
+      <a
+        href="#"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+      >
+        Calendare
+      </a>
+
+      <a
+        href="#"
+        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+      >
+        Reports
+      </a>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
 import "../styles/global.css";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import Shell from "../components/shell/";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCx3AMyKgODCTxDFgDqbUXXj5MICM_MtRA",
+  authDomain: "habits-tracking.firebaseapp.com",
+  projectId: "habits-tracking",
+  storageBucket: "habits-tracking.appspot.com",
+  messagingSenderId: "771015543746",
+  appId: "1:771015543746:web:7a77bf4e17514bc66ea45b",
+  measurementId: "G-R3DNHK8NE0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Component {...pageProps} />;
+      <Shell>
+        <Component {...pageProps} />;
+      </Shell>
     </UserProvider>
   );
 }
