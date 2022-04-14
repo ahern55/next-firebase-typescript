2128144fab6fb62e908b5e26f2b349ae0ae21cdb
Jason Ahern <jason.ahern53@gmail.com>
2022-04-13 21:29:43 -0400

    more setup :D

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Profile() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
   return (
-    user && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.nickname}</p>
-      </div>
-    )
+    <div>
+      <h2>AAA</h2>
+      <p>AA</p>
+    </div>
   );
 }
