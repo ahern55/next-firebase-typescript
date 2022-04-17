a78646e5a4968ba7b712cebb108595237fefb761
Jason Ahern <jason.ahern53@gmail.com>
2022-04-17 19:09:22 -0400

    redirect on login page if logged in

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -16,18 +16,16 @@ const uiConfig = {
 
 function SignInScreen() {
   return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-        <div className="divide-y divide-gray-300/50">
-          <div className="pb-2 text-base font-semibold">
-            Sign in or create a new account
-          </div>
-          <div>
-            <StyledFirebaseAuth
-              uiConfig={uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
-          </div>
+    <div className="relative bg-white px-10 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-lg">
+      <div className="divide-y divide-gray-300/50">
+        <div className="pb-2 text-base font-semibold">
+          Sign in or create a new account
+        </div>
+        <div>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
         </div>
       </div>
     </div>
---
+++ pages/login/Login.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Auth from "../../components/auth";
+import Router from "next/router";
+import firebase from "../../firebase/clientApp";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export default function login() {
+  const [user, loading, error] = useAuthState(firebase.auth());
+
+  if (user) {
+    Router.push("/");
+  }
+
+  return (
+    <>
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-cover bg-gradient-to-r from-blue-500 to-blue-800 py-6 sm:py-6">
+        <Auth />
+      </div>
+    </>
+  );
+}
