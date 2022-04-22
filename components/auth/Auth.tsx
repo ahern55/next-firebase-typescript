afc550875f0dcba55797d0f07a2d23e3f08c14d7
Jason Ahern <jason.ahern53@gmail.com>
2022-04-22 18:17:58 -0400

    splash pageg

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -16,8 +16,8 @@ const uiConfig = {
 
 function SignInScreen() {
   return (
-    <div className="relative bg-white px-10 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-lg">
-      <div className="divide-y divide-gray-300/50">
+    <div className="relative bg-white px-10 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-md">
+      <div className="divide-y divide-indigo-100">
         <div className="pb-2 text-base font-semibold">
           Sign in or create a new account
         </div>
--- components/splash/Splash.tsx
+++ components/splash/Splash.tsx
@@ -1,15 +1,54 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import hero from "../../images/hero.png";
+import LogoSvg from "../../images/svg/logo.svg";
+import styles from "./splash.module.css";
 
 export default function Splash() {
   return (
     <>
-      Splash
-      <button>
-        <Link href={"/login"}>
-          <a>Login</a>
-        </Link>
-      </button>
+      <div className="h-screen pb-14 bg-right bg-cover flex flex-col">
+        <div className="w-full container mx-auto p-6">
+          <div className="w-full flex items-center justify-between">
+            <div className="flex items-center text-blue-300 no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
+              <LogoSvg width="2.5rem" height="2.5rem" className={styles.logo} />
+              <div className="pl-2">My App</div>
+            </div>
+
+            <div className="flex w-1/2 justify-end content-center">
+              <Link href="/login">
+                <a>
+                  <button className="bg-blue-300 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded-full">
+                    Sign in
+                  </button>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="container pt-24 md:pt-12 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+            <h1 className="my-4 text-xl md:text-7xl text-indigo-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">
+              Main Hero message to sell your app
+            </h1>
+            <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">
+              Sub-hero message, not too long and not too short. Make it just
+              right!
+            </p>
+          </div>
+
+          <div className="w-full xl:w-3/5 py-6 xl:pl-48 overflow-y-hidden">
+            <Image src={hero} />
+          </div>
+          <footer>
+            <div className="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+              &copy; My App 2022
+            </div>
+          </footer>
+        </div>
+      </div>
     </>
   );
 }
